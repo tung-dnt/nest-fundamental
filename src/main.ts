924b707a3263df2613213034cc40c2ec39d15b0e
tung-dnt <dongocthanhtung123@gmail.com>
2023-01-19 17:02:47 +0700

    add error handler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from 'src/app/app.module'
+import {NestFactory} from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+import {AppModule} from 'src/app/app.module'
+import {HttpErrorFilter} from './helpers/exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalFilters(new HttpErrorFilter())
+  app.use(cookieParser())
   await app.listen(3000)
 }
 
